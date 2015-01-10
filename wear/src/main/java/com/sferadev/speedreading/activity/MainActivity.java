8a298b49d6380330624d7a63f5ef90fdd4406cb0
SferaDev <sferadev@gmail.com>
2015-01-10 19:06:28 +0100

    Update logic

--- wear/src/main/java/com/sferadev/speedreading/activity/MainActivity.java
+++ wear/src/main/java/com/sferadev/speedreading/activity/MainActivity.java
@@ -8,14 +8,16 @@ import android.view.InputDevice;
 import android.view.InputDevice.MotionRange;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.sferadev.speedreading.R;
 
-import static com.sferadev.speedreading.utils.PreferenceUtils.*;
+import static com.sferadev.speedreading.utils.PreferenceUtils.getPreference;
+import static com.sferadev.speedreading.utils.PreferenceUtils.setPreference;
+import static com.sferadev.speedreading.utils.Utils.KEY_SPEED;
+import static com.sferadev.speedreading.utils.Utils.KEY_TEXT_STRING;
 
 public class MainActivity extends Activity implements OnTouchListener{
     private TextView mTextView;
@@ -23,15 +25,16 @@ public class MainActivity extends Activity implements OnTouchListener{
     private boolean workState = true;
     private int pos = -1;
 
-    private long lastTouch = 0;
-
-    private static String KEY_SPEED = "speed";
+    private float startPoint = 0;
+    private float endPoint = 0;
+    private int lastSpeed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -40,27 +43,39 @@ public class MainActivity extends Activity implements OnTouchListener{
             }
         });
         stub.setOnTouchListener(this);
-        stub.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (lastTouch == 0 || (System.currentTimeMillis() - lastTouch) > 350) {
-                    if (workState) {
-                        workState = false;
-                    } else {
-                        workState = true;
-                        updateText();
-                    }
-                }
-            }
-        });
-        workState = true;
         updateText();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        workState = true;
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         workState = false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        workState = false;
+    }
+
+    private void switchState() {
+        if (workState) {
+            workState = false;
+        } else {
+            workState = true;
+            updateText();
+        }
+    }
+
+    private void updateSpeed(int value) {
+        setPreference(KEY_SPEED, value);
+        Log.d("SpeedReading", "Speed set: " + value);
     }
 
     private void updateText(){
@@ -74,14 +89,14 @@ public class MainActivity extends Activity implements OnTouchListener{
                                 @Override
                                 public void run() {
                                     try {
-                                        mTextView.setText(getString(R.string.lwl).split(" ")[pos]);
+                                        mTextView.setText(getPreference(KEY_TEXT_STRING, getString(R.string.lwl)).split(" ")[pos]);
                                     } catch (ArrayIndexOutOfBoundsException e) {
                                         pos = -1; //Yea I hate myself for this
                                     } catch (NullPointerException e) {
                                         e.printStackTrace();
                                     }
                                     pos++;
-                                    Log.d("SpeedReading", "Position: " + String.valueOf(pos));
+                                    Log.d("SpeedReading", "Word: " + String.valueOf(pos));
                                 }
                             });
                             sleep(getPreference(KEY_SPEED, 800));
@@ -102,14 +117,32 @@ public class MainActivity extends Activity implements OnTouchListener{
     public boolean onTouch(View v, MotionEvent event) {
         InputDevice device = event.getDevice();
         MotionRange range = device.getMotionRange(MotionEvent.AXIS_Y);
+
+        // Check when Input starts and save the start point
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            startPoint = event.getY();
+            Log.d("SpeedReading", "Started on: " + startPoint);
+        }
+
+        // Check when Input point changes and calculate the reading speed
         if (event.getAction() == MotionEvent.ACTION_MOVE) {
             Log.d("SpeedReading", "Moved to: " + event.getY());
-            int value = Math.round(1250 * event.getY() / (range.getMax() - range.getMin() - 20));
-            setPreference(KEY_SPEED, value);
-            Log.d("SpeedReading", "Value set: " + value);
-            lastTouch = System.currentTimeMillis();
-            return true;
+            if (workState) {
+                lastSpeed = Math.round(1250 * event.getY() / (range.getMax() - range.getMin() - 20));
+            }
         }
-        return false;
+
+        // Check when Input ends and determine if was a speed change or click
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            endPoint = event.getY();
+            Log.d("SpeedReading", "Ended on: " + endPoint);
+            Log.d("SpeedReading", "Diff value: " + Math.abs(endPoint - startPoint));
+            if (Math.abs(endPoint - startPoint) > 15) {
+                updateSpeed(lastSpeed);
+            } else {
+                switchState();
+            }
+        }
+        return true;
     }
 }
---
+++ wear/src/main/java/com/sferadev/speedreading/utils/Utils.java
@@ -0,0 +1,8 @@
+package com.sferadev.speedreading.utils;
+
+public class Utils {
+
+    public static final String KEY_SPEED = "speed";
+    public static final String KEY_TEXT_STRING = "textString";
+
+}
